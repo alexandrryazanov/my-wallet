ea73a901c31578713599e557b5d641dd35d42dcf
ryazanov <scooter_zav@mail.ru>
2024-07-01 02:04:25 +0500

    feat(wallets): list and select

--- components/Wallets/index.tsx
+++ components/Wallets/index.tsx
@@ -1,13 +1,20 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Input } from "@nextui-org/input";
 import { Button } from "@nextui-org/button";
-import { child, getDatabase, ref, set } from "@firebase/database";
-import { getAuth } from "firebase/auth";
+import { child, get, getDatabase, ref, set } from "@firebase/database";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { format } from "date-fns";
+import { Select, SelectItem, SelectSection } from "@nextui-org/select";
+import { Selection } from "@nextui-org/table";
+import { Listbox, ListboxItem } from "@nextui-org/listbox";
 
 const Wallets = () => {
+  const [existedWallets, setExistedWallets] = useState<{ name: string }[]>([]);
+  const [value, setValue] = React.useState<Selection>(new Set([]));
+  const [inputVisible, setInputVisible] = useState(false);
+
   const [name, setName] = useState("");
 
   const onAdd = () => {
@@ -30,18 +37,91 @@ const Wallets = () => {
     }
   };
 
+  useEffect(() => {
+    const auth = getAuth();
+
+    const getWallets = async () => {
+      if (!auth.currentUser) return;
+
+      try {
+        const dbRef = ref(getDatabase());
+
+        const snapshot = await get(
+          child(dbRef, `data/${auth.currentUser.uid}`),
+        );
+
+        if (!snapshot.exists()) return;
+
+        const results = snapshot.val();
+        const walletNames = Object.values(results).reduce<string[]>(
+          (acc, { wallets }: any) => [...acc, ...Object.keys(wallets)],
+          [],
+        );
+
+        setExistedWallets(walletNames.map((name) => ({ name })));
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    onAuthStateChanged(auth, (user) => {
+      if (!user) return;
+      getWallets();
+    });
+  }, []);
+
+  const onSelect = (keys: Selection) => {
+    if (typeof keys === "string") return;
+    setInputVisible(keys.has("new"));
+
+    setValue(keys);
+  };
+
   return (
     <div className={"h-full"}>
-      {/*TODO list*/}
-      <div className={"flex w-full flex-wrap md:flex-nowrap gap-4"}>
-        <Input
-          type="name"
-          size={"md"}
-          placeholder="Enter wallet name"
-          minLength={2}
-          value={name}
-          onValueChange={setName}
-        />
+      <Listbox aria-label="Wallets" onAction={(key) => alert(key)}>
+        {existedWallets.map((wallet) => (
+          <ListboxItem key={wallet.name}>{wallet.name}</ListboxItem>
+        ))}
+      </Listbox>
+
+      <div className={"flex gap-2 flex-wrap"}>
+        <Select
+          placeholder="Select a wallet"
+          className="w-5/12"
+          selectedKeys={value}
+          onSelectionChange={onSelect}
+          aria-label={"Select Wallet"}
+        >
+          <SelectSection showDivider>
+            {existedWallets.map((wallet) => (
+              <SelectItem
+                key={wallet.name}
+                textValue={wallet.name}
+                aria-label={wallet.name}
+              >
+                {wallet.name}
+              </SelectItem>
+            ))}
+          </SelectSection>
+
+          <SelectSection>
+            <SelectItem key={"new"} textValue={"Enter name..."}>
+              Create a new wallet
+            </SelectItem>
+          </SelectSection>
+        </Select>
+        {inputVisible && (
+          <Input
+            type="name"
+            size={"md"}
+            placeholder="Enter wallet name"
+            minLength={2}
+            value={name}
+            onValueChange={setName}
+            className={"w-4/12"}
+          />
+        )}
         <Button onClick={onAdd}>Add</Button>
       </div>
     </div>
